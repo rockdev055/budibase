93303b57e5f854db7f18ea0ec42aa9e971747ba5
rockdev55 <steve264225@gmail.com>
2020-04-17 14:24:51 +0100

    Components - icons, descriptions and other tidyup

---
+++ packages/builder/src/components/userInterface/ComponentTab/Tab.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Item from "./Item.svelte"
+  export let components
+</script>
+
+{#each components as { icon, name, description }}
+  <Item {icon} {name} {description} />
+{/each}
