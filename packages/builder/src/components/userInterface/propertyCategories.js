0ebbdd34776621ca84d7982bb467e92d148a5689
rockdev55 <steve264225@gmail.com>
2020-06-02 07:56:50 +0100

    auto select model after creation

--- packages/builder/src/components/userInterface/propertyCategories.js
+++ packages/builder/src/components/userInterface/propertyCategories.js
@@ -11,8 +11,9 @@ export const layout = [
     label: "Display",
     key: "display",
     control: OptionSelect,
-    initialValue: "Flex",
+    initialValue: "Select Option",
     options: [
+      { label: "Select Option", value: "" },
       { label: "Flex", value: "flex" },
       { label: "Inline Flex", value: "inline-flex" },
     ],
@@ -21,11 +22,12 @@ export const layout = [
     label: "Direction",
     key: "flex-direction",
     control: OptionSelect,
-    initialValue: "Row",
+    initialValue: "Select Option",
     options: [
+      { label: "Select Option", value: "" },
       { label: "Row", value: "row" },
       { label: "Row Reverse", value: "rowReverse" },
-      { label: "column", value: "column" },
+      { label: "Column", value: "column" },
       { label: "Column Reverse", value: "columnReverse" },
     ],
   },
@@ -33,8 +35,9 @@ export const layout = [
     label: "Justify",
     key: "justify-content",
     control: OptionSelect,
-    initialValue: "Flex Start",
+    initialValue: "Select Option",
     options: [
+      { label: "Select Option", value: "" },
       { label: "Flex Start", value: "flex-start" },
       { label: "Flex End", value: "flex-end" },
       { label: "Center", value: "center" },
@@ -47,8 +50,9 @@ export const layout = [
     label: "Align",
     key: "align-items",
     control: OptionSelect,
-    initialValue: "Flex Start",
+    initialValue: "Select Option",
     options: [
+      { label: "Select Option", value: "" },
       { label: "Flex Start", value: "flex-start" },
       { label: "Flex End", value: "flex-end" },
       { label: "Center", value: "center" },
@@ -60,20 +64,21 @@ export const layout = [
     label: "Wrap",
     key: "flex-wrap",
     control: OptionSelect,
-    initialValue: "Wrap",
+    initialValue: "Select Option",
     options: [
-      { label: "Wrap", value: "wrap" },
+      { label: "Select Option", value: "" },
       { label: "No Wrap", value: "nowrap" },
+      { label: "Wrap", value: "wrap" },
       { label: "Wrap Reverse", value: "wrap-reverse" },
     ],
   },
 ]
 
 const spacingMeta = [
-  { placeholder: "L" },
-  { placeholder: "B" },
-  { placeholder: "R" },
   { placeholder: "T" },
+  { placeholder: "R" },
+  { placeholder: "B" },
+  { placeholder: "L" },
 ]
 
 export const spacing = [
@@ -195,10 +200,10 @@ export const typography = [
     label: "Decoration",
     key: "text-decoration-line",
     control: OptionSelect,
-    defaultValue: "Underline",
+    defaultValue: "None",
     options: [
-      { label: "Underline", value: "underline" },
       { label: "None", value: "none" },
+      { label: "Underline", value: "underline" },
       { label: "Overline", value: "overline" },
       { label: "Line-through", value: "line-through" },
       { label: "Under Over", value: "underline overline" },
