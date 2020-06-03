f8a00c7cc92a29a8685de4d27dde23baaf3166c1
rockdev55 <steve264225@gmail.com>
2020-06-03 09:25:56 +0100

    replace DataList with new List

--- packages/builder/src/components/userInterface/temporaryPanelStructure.js
+++ packages/builder/src/components/userInterface/temporaryPanelStructure.js
@@ -307,9 +307,20 @@ export default {
           },
           children: [],
         },
-        {
+        /*{
           name: "List",
           _component: "@budibase/standard-components/datalist",
+          description: "Shiny list",
+          icon: "ri-file-list-fill",
+          properties: {
+            design: { ...all },
+            settings: [{ label: "Model", key: "model", control: ModelSelect }],
+          },
+          children: [],
+        },*/
+        {
+          name: "List",
+          _component: "@budibase/standard-components/list",
           description: "Shiny list",
           icon: "ri-file-list-fill",
           properties: {
