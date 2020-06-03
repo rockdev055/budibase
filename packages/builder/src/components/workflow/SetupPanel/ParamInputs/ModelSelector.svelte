3fe6194f88c5c681b55c64175f8bcc2f2b1a195a
rockdev55 <steve264225@gmail.com>
2020-06-03 14:36:39 +0200

    adds options to navbar component

--- packages/builder/src/components/workflow/SetupPanel/ParamInputs/ModelSelector.svelte
+++ packages/builder/src/components/workflow/SetupPanel/ParamInputs/ModelSelector.svelte
@@ -7,7 +7,6 @@
 <div class="uk-margin block-field">
   <div class="uk-form-controls">
     <select class="budibase__input" bind:value>
-      <option value=""></option>
       {#each $backendUiStore.models as model}
         <option value={model}>{model.name}</option>
       {/each}
