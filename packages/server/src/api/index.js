988564a8c617f4f30b1b6724bd51683abba4c998
rockdev55 <steve264225@gmail.com>
2020-07-08 17:14:23 +0100

    Functional Chart Wrapper for Britecharts

--- packages/server/src/api/index.js
+++ packages/server/src/api/index.js
@@ -7,7 +7,6 @@ const {
   authRoutes,
   pageRoutes,
   userRoutes,
-  deployRoutes,
   instanceRoutes,
   clientRoutes,
   applicationRoutes,
@@ -18,7 +17,6 @@ const {
   componentRoutes,
   workflowRoutes,
   accesslevelRoutes,
-  apiKeysRoutes,
 } = require("./routes")
 
 const router = new Router()
@@ -83,9 +81,6 @@ router.use(instanceRoutes.allowedMethods())
 
 router.use(workflowRoutes.routes())
 router.use(workflowRoutes.allowedMethods())
-
-router.use(deployRoutes.routes())
-router.use(deployRoutes.allowedMethods())
 // end auth routes
 
 router.use(pageRoutes.routes())
@@ -102,9 +97,6 @@ router.use(clientRoutes.allowedMethods())
 
 router.use(accesslevelRoutes.routes())
 router.use(accesslevelRoutes.allowedMethods())
-
-router.use(apiKeysRoutes.routes())
-router.use(apiKeysRoutes.allowedMethods())
 
 router.use(staticRoutes.routes())
 router.use(staticRoutes.allowedMethods())
