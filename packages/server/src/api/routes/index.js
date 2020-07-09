c3531f032572ebc9fb9bd1183159e50694fc0799
rockdev55 <steve264225@gmail.com>
2020-07-09 10:39:49 +0100

    Adding CDN links and Tidyup

--- packages/server/src/api/routes/index.js
+++ packages/server/src/api/routes/index.js
@@ -11,11 +11,8 @@ const staticRoutes = require("./static")
 const componentRoutes = require("./component")
 const workflowRoutes = require("./workflow")
 const accesslevelRoutes = require("./accesslevel")
-const deployRoutes = require("./deploy")
-const apiKeysRoutes = require("./apikeys")
 
 module.exports = {
-  deployRoutes,
   authRoutes,
   pageRoutes,
   userRoutes,
@@ -29,5 +26,4 @@ module.exports = {
   componentRoutes,
   workflowRoutes,
   accesslevelRoutes,
-  apiKeysRoutes,
 }
