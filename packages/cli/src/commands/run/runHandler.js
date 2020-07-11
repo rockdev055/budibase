bd64c2f3652e1aba45259e068411920ac10f5819
rockdev55 <steve264225@gmail.com>
2020-07-11 17:54:13 +0200

    avoiding ctrl+c

--- packages/cli/src/commands/run/runHandler.js
+++ packages/cli/src/commands/run/runHandler.js
@@ -1,13 +1,13 @@
 const { xPlatHomeDir } = require("../../common")
 const { resolve } = require("path")
 
-module.exports = async ({ dir }) => {
+module.exports = ({ dir }) => {
   dir = xPlatHomeDir(dir)
   process.env.BUDIBASE_DIR = resolve(dir)
   require("dotenv").config({ path: resolve(dir, ".env") })
 
   // dont make this a variable or top level require
-  // it will cause environment module to be loaded prematurely
+  // ti will cause environment module to be loaded prematurely
   return require("@budibase/server/src/app")().then(server => {
     server.on("close", () => console.log("Server Closed"))
     console.log(`Budibase running on ${JSON.stringify(server.address())}`)
--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,22 +1,13 @@
-const { resolve, join } = require("path")
-const { homedir } = require("os")
+const { resolve } = require("path")
 
 async function runServer() {
-  const budibaseDir = join(homedir(), ".budibase"); 
-  process.env.BUDIBASE_DIR = budibaseDir 
+  const budibaseDir = "~/.budibase"
 
-  require("dotenv").config({ path: resolve(budibaseDir, ".env") })
+  process.env.BUDIBASE_DIR = resolve(budibaseDir)
 
   const server = await require("./app")()
-
   server.on("close", () => console.log("Server Closed"))
   console.log(`Budibase running on ${JSON.stringify(server.address())}`)
 }
 
 runServer()
-
-
-process.on("SIGINT", function() {
-  console.log("Shutting down server.." );
-  process.exit(1);
-});
