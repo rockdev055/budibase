aecca595c240850145bffda1fcdf0b59d9d6d9d6
rockdev55 <steve264225@gmail.com>
2020-07-31 10:46:23 +0200

    setup sidebar

--- packages/standard-components/src/Chart/index.js
+++
@@ -1,9 +0,0 @@
-export { default as donut } from "./Donut.svelte"
-export { default as bar } from "./Bar.svelte"
-export { default as line } from "./Line.svelte"
-export { default as brush } from "./Brush.svelte"
-export { default as bullet } from "./Bullet.svelte"
-export { default as groupedbar } from "./GroupedBar.svelte"
-export { default as heatmap } from "./Heatmap.svelte"
-export { default as sparkline } from "./Sparkline.svelte"
-
