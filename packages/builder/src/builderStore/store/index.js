ebfac4f523ae2117b63f4a757743caa65b4ac5e6
rockdev55 <steve264225@gmail.com>
2020-08-07 14:46:00 +0100

    Tidyup

--- packages/builder/src/builderStore/store/index.js
+++ packages/builder/src/builderStore/store/index.js
@@ -1,5 +1,5 @@
 import { values } from "lodash/fp"
-import getNewComponentName from "../getNewComponentName"
+import { get_capitalised_name } from "../../helpers"
 import { backendUiStore } from "builderStore"
 import * as backendStoreActions from "./backend"
 import { writable, get } from "svelte/store"
@@ -255,7 +255,7 @@ const setCurrentPage = store => pageName => {
  * @param  {string} componentToAdd - name of the component to add to the application
  * @param  {string} presetName - name of the component preset if defined
  */
-const addChildComponent = store => (componentToAdd, presetName) => {
+const addChildComponent = store => (componentToAdd, presetProps = {}) => {
   store.update(state => {
     function findSlot(component_array) {
       for (let i = 0; i < component_array.length; i += 1) {
@@ -278,10 +278,8 @@ const addChildComponent = store => (componentToAdd, presetName) => {
 
     const component = getComponentDefinition(state, componentToAdd)
 
-    const presetProps = presetName ? component.presets[presetName] : {}
-
     const instanceId = get(backendUiStore).selectedDatabase._id
-    const instanceName = getNewComponentName(componentToAdd, state)
+    const instanceName = get_capitalised_name(componentToAdd)
 
     const newComponent = createProps(
       component,
