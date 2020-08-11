6a5435e33b49df52f53b6bd768f947911b08ad94
rockdev55 <steve264225@gmail.com>
2020-08-11 17:20:55 +0100

    small styling fix

--- packages/builder/src/components/start/Steps/User.svelte
+++ packages/builder/src/components/start/Steps/User.svelte
@@ -21,7 +21,7 @@
     placeholder="Password"
     type="pasword"
     error={blurred.password && validationErrors.password} />
-  <Select secondary name="accessLevelId">
+  <Select name="accessLevelId">
     <option value="ADMIN">Admin</option>
     <option value="POWER_USER">Power User</option>
   </Select>
