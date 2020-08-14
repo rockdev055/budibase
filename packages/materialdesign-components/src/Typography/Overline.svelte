ff85a0f701a60b09e2ffba7c7d99fe78e3dbc2cf
rockdev55 <steve264225@gmail.com>
2020-08-14 14:21:55 +0100

    removed PageView.svelte

---
+++ packages/materialdesign-components/src/Typography/Overline.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let text = ""
+  export let verticalMargin = 0
+  export let horizontalMargin = 0
+</script>
+
+<span
+  style={`margin: ${verticalMargin}px ${horizontalMargin}px`}
+  class="mdc-typography--overline">
+  {text}
+</span>
