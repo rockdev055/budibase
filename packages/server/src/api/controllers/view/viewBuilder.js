5d56f4e5b13f6b3187decbed3d08d80951a907e4
rockdev55 <steve264225@gmail.com>
2020-08-21 17:05:26 +0100

    custom filtering working, needs more test coverage

--- packages/server/src/api/controllers/view/viewBuilder.js
+++ packages/server/src/api/controllers/view/viewBuilder.js
@@ -1,9 +1,58 @@
-function statsViewTemplate({ field, modelId, groupBy }) {
+const TOKEN_MAP = {
+  EQUALS: "===",
+  LT: "<",
+  LTE: "<=",
+  MT: ">",
+  MTE: ">=",
+  CONTAINS: "includes",
+  AND: "&&",
+  OR: "||",
+}
+
+/**
+ * Iterates through the array of filters to create a JS
+ * expression that gets used in a CouchDB view.
+ * @param {Array} filters - an array of filter objects
+ * @returns {String} JS Expression
+ */
+function parseFilterExpression(filters) {
+  const expression = []
+
+  for (let filter of filters) {
+    if (filter.conjunction) expression.push(TOKEN_MAP[filter.conjunction]);
+
+    if (filter.condition === "CONTAINS") {
+      expression.push(
+        `doc["${filter.key}"].${TOKEN_MAP[filter.condition]}("${
+        filter.value
+      }")`)
+      return
+    }
+
+    expression.push(`doc["${filter.key}"] ${TOKEN_MAP[filter.condition]} "${
+      filter.value
+    }"`)
+  }
+
+  return expression.join(" ")
+}
+
+function parseEmitExpression(field, groupBy) {
+  if (field) return `emit(doc["${groupBy || "_id"}"], doc["${field}"]);`
+  return `emit(doc._id);`
+}
+
+function statsViewTemplate({ field, modelId, groupBy, filters = [] }) {
+  const filterExpression = parseFilterExpression(filters)
+
+  const emitExpression = parseEmitExpression(field, groupBy)
+
   return {
     meta: {
       field,
       modelId,
       groupBy,
+      filters,
       schema: {
         sum: "number",
         min: "number",
@@ -14,8 +63,10 @@ function statsViewTemplate({ field, modelId, groupBy }) {
       },
     },
     map: `function (doc) {
-      if (doc.modelId === "${modelId}") {
-        emit(doc["${groupBy || "_id"}"], doc["${field}"]);  
+      if (doc.modelId === "${modelId}" ${
+      filterExpression ? `&& ${filterExpression}` : ""
+    }) {
+        ${emitExpression}
       }
     }`,
     reduce: "_stats",
