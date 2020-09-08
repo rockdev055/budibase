a8719c0004fcc7b568bc21c921edf561fda6a61a
rockdev55 <steve264225@gmail.com>
2020-09-08 18:38:47 +0100

    Fixing an issue where the buttons for the components hierarchy were overflowing outside of their parent panel, removing the specification of the width and height allows computed values to be used instead (including padding etc) - check this fixes it in windows, linux and electron app.

--- packages/builder/src/components/common/DatePicker.svelte
+++ packages/builder/src/components/common/DatePicker.svelte
@@ -1,23 +1,32 @@
 <script>
-  // import flatpickr from "flatpickr"
-  // import "flatpickr/dist/flatpickr.css"
-  // import { onMount } from "svelte"
+  import flatpickr from "flatpickr"
+  import "flatpickr/dist/flatpickr.css"
+  import { onMount } from "svelte"
   import { Label, Input } from "@budibase/bbui"
 
+  export let value
   export let label
-  export let datetime
 
-  let date = datetime.substring(0, 10)
-  let time = datetime.substring(11, 16)
+  let input
+  let fpInstance
 
-  $: console.log({ datetime, date, time })
+  $: if (fpInstance) fpInstance.setDate(value)
 
-  // TODO: update to use proper date parsing logic - dayJS or similar
-  $: datetime = `${date}T${time}:00.000Z`
+  onMount(() => {
+    fpInstance = flatpickr(input, {})
+
+    fpInstance.config.onChange.push(selectedDates => {
+      if (selectedDates.length > 0) value = new Date(selectedDates[0])
+    })
+
+    return fpInstance
+  })
 </script>
 
 <div class="bb-margin-m">
   <Label small forAttr={'datepicker-label'}>{label}</Label>
-  <input type="date" bind:value={date} />
-  <input type="time" bind:value={time} />
+  <Input thin bind:this={input} />
 </div>
+
+<!-- TODO: Verify DatePicker Input works as expected when datetime property used again
+in CreateEditColumn -->
--- packages/builder/src/components/database/DataTable/modals/CreateEditColumn.svelte
+++ packages/builder/src/components/database/DataTable/modals/CreateEditColumn.svelte
@@ -83,11 +83,11 @@
         bind:values={field.constraints.inclusion} />
     {:else if field.type === 'datetime' && field.constraints}
       <DatePicker
-        label="Earliest"
-        bind:datetime={field.constraints.datetime.earliest} />
+        label="Min Value"
+        bind:value={field.constraints.datetime.earliest} />
       <DatePicker
-        label="Latest"
-        bind:datetime={field.constraints.datetime.latest} />
+        label="Max Value"
+        bind:value={field.constraints.datetime.latest} />
     {:else if field.type === 'number' && field.constraints}
       <NumberBox
         label="Min Value"
