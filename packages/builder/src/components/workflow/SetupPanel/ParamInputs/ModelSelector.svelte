0d6a58abeac3b14f5dfa51a18ee66275d7e2dc8a
rockdev55 <steve264225@gmail.com>
2020-09-18 16:19:41 +0100

    lowercase file extension

--- packages/builder/src/components/workflow/SetupPanel/ParamInputs/ModelSelector.svelte
+++ packages/builder/src/components/workflow/SetupPanel/ParamInputs/ModelSelector.svelte
@@ -1,15 +1,23 @@
 <script>
   import { backendUiStore } from "builderStore"
-  import { Select } from "@budibase/bbui"
 
   export let value
+  $: modelId = value ? value._id : ""
+
+  function onChange(e) {
+    value = $backendUiStore.models.find(model => model._id === e.target.value)
+  }
 </script>
 
 <div class="block-field">
-  <Select bind:value secondary thin>
+  <select
+    class="budibase__input"
+    value={modelId}
+    on:blur={onChange}
+    on:change={onChange}>
     <option value="">Choose an option</option>
     {#each $backendUiStore.models as model}
       <option value={model._id}>{model.name}</option>
     {/each}
-  </Select>
+  </select>
 </div>
