26e202f603fd1bafe06162d36c9fc54720de27d8
rockdev55 <steve264225@gmail.com>
2020-10-01 11:01:06 +0100

    Add functional backend UI for navigating relationships

--- packages/builder/src/components/settings/tabs/APIKeys.svelte
+++ packages/builder/src/components/settings/tabs/APIKeys.svelte
@@ -3,21 +3,13 @@
   import { store } from "builderStore"
   import api from "builderStore/api"
   import posthog from "posthog-js"
-  import analytics from "analytics"
 
   let keys = { budibase: "", sendGrid: "" }
 
   async function updateKey([key, value]) {
-    if (key === "budibase") {
-      const isValid = await analytics.identifyByApiKey(value)
-      if (!isValid) {
-        // TODO: add validation message
-        keys = { ...keys }
-        return
-      }
-    }
     const response = await api.put(`/api/keys/${key}`, { value })
     const res = await response.json()
+    if (key === "budibase") posthog.identify(value)
     keys = { ...keys, ...res }
   }
 
@@ -25,8 +17,6 @@
   async function fetchKeys() {
     const response = await api.get(`/api/keys/`)
     const res = await response.json()
-    // dont want this to ever be editable, as its fetched based on Api Key
-    if (res.userId) delete res.userId
     keys = res
   }
 
@@ -34,31 +24,23 @@
 </script>
 
 <div class="container">
-  <div class="background">
-    <Input
-      on:save={e => updateKey(['budibase', e.detail])}
-      thin
-      edit
-      value={keys.budibase}
-      label="Budibase" />
-  </div>
-  <div class="background">
-    <Input
-      on:save={e => updateKey(['sendgrid', e.detail])}
-      thin
-      edit
-      value={keys.sendgrid}
-      label="Sendgrid" />
-  </div>
+  <Input
+    on:save={e => updateKey(['budibase', e.detail])}
+    thin
+    edit
+    value={keys.budibase}
+    label="Budibase API Key" />
+  <Input
+    on:save={e => updateKey(['sendgrid', e.detail])}
+    thin
+    edit
+    value={keys.sendgrid}
+    label="Sendgrid API Key" />
 </div>
 
 <style>
   .container {
     display: grid;
-    grid-gap: var(--space);
-  }
-  .background {
-    border-radius: 5px;
-    padding: 12px 0px;
+    grid-gap: var(--spacing-xl);
   }
 </style>
