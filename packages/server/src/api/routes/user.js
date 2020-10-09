980c5ee62091ce169d034b3c31493c56c351ed61
rockdev55 <steve264225@gmail.com>
2020-10-09 17:12:55 +0100

    renaming Record > Row

--- packages/server/src/api/routes/user.js
+++ packages/server/src/api/routes/user.js
@@ -2,7 +2,6 @@ const Router = require("@koa/router")
 const controller = require("../controllers/user")
 const authorized = require("../../middleware/authorized")
 const { USER_MANAGEMENT, LIST_USERS } = require("../../utilities/accessLevels")
-const usage = require("../../middleware/usageQuota")
 
 const router = Router()
 
@@ -10,11 +9,10 @@ router
   .get("/api/users", authorized(LIST_USERS), controller.fetch)
   .get("/api/users/:username", authorized(USER_MANAGEMENT), controller.find)
   .put("/api/users/", authorized(USER_MANAGEMENT), controller.update)
-  .post("/api/users", authorized(USER_MANAGEMENT), usage, controller.create)
+  .post("/api/users", authorized(USER_MANAGEMENT), controller.create)
   .delete(
     "/api/users/:username",
     authorized(USER_MANAGEMENT),
-    usage,
     controller.destroy
   )
 
