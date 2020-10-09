09d530293e7173caee38dc440e4964e4d2828590
rockdev55 <steve264225@gmail.com>
2020-10-09 11:32:25 +0100

    Adding in default plan if the obj found does not have the required information.

--- packages/server/src/api/controllers/deploy/index.js
+++ packages/server/src/api/controllers/deploy/index.js
@@ -1,11 +1,6 @@
 const CouchDB = require("pouchdb")
 const PouchDB = require("../../../db")
-const {
-  uploadAppAssets,
-  verifyDeployment,
-  updateDeploymentQuota,
-} = require("./aws")
-const { DocumentTypes } = require("../../../db/utils")
+const { uploadAppAssets, fetchTemporaryCredentials } = require("./aws")
 
 function replicate(local, remote) {
   return new Promise((resolve, reject) => {
@@ -36,44 +31,13 @@ async function replicateCouch({ instanceId, clientId, credentials }) {
   await Promise.all(replications)
 }
 
-async function getCurrentInstanceQuota(instanceId) {
-  const db = new PouchDB(instanceId)
-  const records = await db.allDocs({
-    startkey: "re:",
-    endkey: `re:\ufff0`,
-  })
-  const users = await db.allDocs({
-    startkey: DocumentTypes.USER + ,
-    endkey: `us:\ufff0`,
-  })
-  const existingRecords = records.rows.length
-
-  const designDoc = await db.get("_design/database")
-
-  return {
-    records: existingRecords,
-    views: Object.keys(designDoc.views).length,
-  }
-}
-
 exports.deployApp = async function(ctx) {
   try {
     const clientAppLookupDB = new PouchDB("client_app_lookup")
     const { clientId } = await clientAppLookupDB.get(ctx.user.appId)
 
-    const instanceQuota = await getCurrentInstanceQuota(ctx.user.instanceId)
-    const credentials = await verifyDeployment({
-      instanceId: ctx.user.instanceId,
-      appId: ctx.user.appId,
-      quota: instanceQuota,
-    })
-
     ctx.log.info(`Uploading assets for appID ${ctx.user.appId} assets to s3..`)
-
-    if (credentials.errors) {
-      ctx.throw(500, credentials.errors)
-      return
-    }
+    const credentials = await fetchTemporaryCredentials()
 
     await uploadAppAssets({
       clientId,
@@ -89,11 +53,6 @@ exports.deployApp = async function(ctx) {
       clientId,
       credentials: credentials.couchDbCreds,
     })
-
-    const deployedInstanceQuota = await getCurrentInstanceQuota(
-      ctx.user.instanceId
-    )
-    await updateDeploymentQuota(deployedInstanceQuota)
 
     ctx.body = {
       status: "SUCCESS",
