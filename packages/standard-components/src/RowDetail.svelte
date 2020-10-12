58cd89ef7be4eef988a95cbdc792f8597954ae47
rockdev55 <steve264225@gmail.com>
2020-10-12 14:14:50 +0100

    Replace bindings to link fields with new count runtime property

--- packages/standard-components/src/RowDetail.svelte
+++ packages/standard-components/src/RowDetail.svelte
@@ -20,40 +20,49 @@
     if (response.status === 200) {
       const allRecords = await response.json()
       if (allRecords.length > 0) return allRecords[0]
+      return { modelId: model }
     }
   }
 
   async function fetchData() {
     const pathParts = window.location.pathname.split("/")
 
+    if (!model) {
+      return
+    }
+
     let record
     // if srcdoc, then we assume this is the builder preview
     if (pathParts.length === 0 || pathParts[0] === "srcdoc") {
-      record = await fetchFirstRecord()
-    } else {
-      const id = pathParts[pathParts.length - 1]
-      const GET_RECORD_URL = `/api/${model}/records/${id}`
+      if (model) record = await fetchFirstRecord()
+    } else if (_bb.routeParams().id) {
+      const GET_RECORD_URL = `/api/${model}/records/${_bb.routeParams().id}`
       const response = await _bb.api.get(GET_RECORD_URL)
       if (response.status === 200) {
         record = await response.json()
+      } else {
+        throw new Error("Failed to fetch record.", response)
       }
+    } else {
+      throw new Exception("Record ID was not supplied to RowDetail")
     }
 
     if (record) {
       // Fetch model schema so we can check for linked records
-      const model = await fetchModel(record.modelId)
-      for (let key of Object.keys(model.schema)) {
-        if (model.schema[key].type === "link") {
+      const modelObj = await fetchModel(record.modelId)
+      for (let key of Object.keys(modelObj.schema)) {
+        if (modelObj.schema[key].type === "link") {
           record[key] = Array.isArray(record[key]) ? record[key].length : 0
         }
       }
 
+      record._model = modelObj
+
       _bb.attachChildren(target, {
-        hydrate: false,
         context: record,
       })
     } else {
-      throw new Error("Failed to fetch record.", response)
+      _bb.attachChildren(target)
     }
   }
 
