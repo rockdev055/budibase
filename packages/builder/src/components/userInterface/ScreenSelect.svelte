61d084106dca3c2d1c54497927aed8c077c30924
rockdev55 <steve264225@gmail.com>
2020-10-12 16:37:08 +0100

    Fixes for deleting records when a table is deleted.

--- packages/builder/src/components/userInterface/ScreenSelect.svelte
+++ packages/builder/src/components/userInterface/ScreenSelect.svelte
@@ -10,22 +10,9 @@
   const handleBlur = () => dispatch("change", value)
 </script>
 
-<div>
-  <DataList editable secondary thin on:blur={handleBlur} on:change bind:value>
-    <option value="" />
-    {#each $store.allScreens as screen}
-      <option value={screen.route}>{screen.props._instanceName}</option>
-    {/each}
-  </DataList>
-</div>
-
-<style>
-  div {
-    flex: 1 1 auto;
-    display: flex;
-    flex-direction: row;
-  }
-  div :global(> div) {
-    flex: 1 1 auto;
-  }
-</style>
+<DataList editable secondary on:blur={handleBlur} on:change bind:value>
+  <option value="" />
+  {#each $store.allScreens as screen}
+    <option value={screen.route}>{screen.props._instanceName}</option>
+  {/each}
+</DataList>
