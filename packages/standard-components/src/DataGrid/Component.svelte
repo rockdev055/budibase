4b813b10f0ddc50542c1fba46138b5c9f4131a6a
rockdev55 <steve264225@gmail.com>
2020-10-13 11:18:34 +0100

    removing z-index from checkbox

--- packages/standard-components/src/DataGrid/Component.svelte
+++ packages/standard-components/src/DataGrid/Component.svelte
@@ -18,7 +18,7 @@
   export let datasource = {}
   export let editable
   export let theme = "alpine"
-  export let height = 500
+  export let height
   export let pagination
 
   let dataLoaded = false
@@ -115,7 +115,7 @@
     href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" />
 </svelte:head>
 
-<div style="--grid-height: {height}px">
+<div class="container" style="--grid-height: {height}px">
   {#if dataLoaded}
     {#if editable}
       <div class="controls">
@@ -139,6 +139,9 @@
 </div>
 
 <style>
+  .container {
+    --grid-height: 800px;
+  }
   .container :global(form) {
     display: grid;
     grid-template-columns: repeat(2);
