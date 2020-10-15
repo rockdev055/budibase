a99cceaa48040e0e3a7f3aa82eb25523126a685b
rockdev55 <steve264225@gmail.com>
2020-10-15 12:09:41 +0100

    more filtering updates

--- packages/standard-components/src/DataGrid/customRenderer.js
+++ packages/standard-components/src/DataGrid/customRenderer.js
@@ -4,7 +4,6 @@
 import AttachmentCell from "./AttachmentCell/Button.svelte"
 import Select from "./Select/Wrapper.svelte"
 import DatePicker from "./DateTime/Wrapper.svelte"
-import RelationshipDisplay from "./Relationship/RelationshipDisplay.svelte"
 
 const renderers = new Map([
   ["boolean", booleanRenderer],
@@ -104,20 +103,14 @@ function optionsRenderer({ inclusion }, editable) {
   }
 }
 /* eslint-disable no-unused-vars */
-function linkedRowRenderer(constraints, editable, schema) {
+function linkedRowRenderer(constraints, editable) {
   return params => {
     let container = document.createElement("div")
     container.style.display = "grid"
     container.style.placeItems = "center"
     container.style.height = "100%"
 
-    new RelationshipDisplay({
-      target: container,
-      props: {
-        row: params.data,
-        columnName: params.column.colId,
-      },
-    })
+    container.innerText = params.value ? params.value.length : 0
 
     return container
   }
