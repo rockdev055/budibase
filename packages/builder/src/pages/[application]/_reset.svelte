d2b818b2f401d68edc987126c603c0cd13a3e4af
rockdev55 <steve264225@gmail.com>
2020-10-21 17:35:39 +0100

    Adding validation to pages to protect against screen/page creation with no parameters.

--- packages/builder/src/pages/[application]/_reset.svelte
+++ packages/builder/src/pages/[application]/_reset.svelte
@@ -2,7 +2,6 @@
   import { store, automationStore, backendUiStore } from "builderStore"
   import { Button } from "@budibase/bbui"
   import SettingsLink from "components/settings/Link.svelte"
-  import Feedback from "components/userInterface/Feedback.svelte"
   import { get } from "builderStore/api"
   import { isActive, goto, layout } from "@sveltech/routify"
   import { PreviewIcon } from "components/common/Icons/"
@@ -66,7 +65,6 @@
       {/each}
     </div>
     <div class="toprightnav">
-      <Feedback />
       <SettingsLink />
       <span
         class:active={false}
