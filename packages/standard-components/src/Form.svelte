2e2efbedd3f852921b1a0060b6a635d5593bdb1f
rockdev55 <steve264225@gmail.com>
2020-11-23 12:12:03 +0000

    fix fast view switching bug

--- packages/standard-components/src/Form.svelte
+++ packages/standard-components/src/Form.svelte
@@ -1,63 +1,54 @@
 <script>
-  import { getContext } from "svelte"
   import { Label, DatePicker, Input, Select, Toggle } from "@budibase/bbui"
   import Dropzone from "./attachments/Dropzone.svelte"
   import LinkedRowSelector from "./LinkedRowSelector.svelte"
+  import ErrorsBox from "./ErrorsBox.svelte"
   import { capitalise } from "./helpers"
 
-  const { styleable, screenStore, API } = getContext("sdk")
-  const dataContextStore = getContext("data")
-  const styles = getContext("style")
-
+  export let _bb
+  export let table
   export let wide = false
 
-  let row
-  let schema
-  let fields = []
+  let store = _bb.store
+  let schema = {}
+  let rowId
+  let errors = {}
 
-  $: getContextDetails($dataContextStore)
-
-  const getContextDetails = async dataContext => {
-    row = dataContext?.data
-    if (row) {
-      const tableDefinition = await API.fetchTableDefinition(row.tableId)
-      schema = tableDefinition.schema
-      fields = Object.keys(schema)
-    }
-  }
+  $: schema = $store.data && $store.data._table && $store.data._table.schema
+  $: fields = schema ? Object.keys(schema) : []
 </script>
 
-<div class="form-content" use:styleable={styles}>
-  <!--  <ErrorsBox errors={$store.saveRowErrors || {}} />-->
+<div class="form-content">
+  <ErrorsBox errors={$store.saveRowErrors || {}} />
   {#each fields as field}
     <div class="form-field" class:wide>
       {#if !(schema[field].type === 'boolean' && !wide)}
         <Label extraSmall={!wide} grey>{capitalise(schema[field].name)}</Label>
       {/if}
       {#if schema[field].type === 'options'}
-        <Select secondary bind:value={row[field]}>
+        <Select secondary bind:value={$store.data[field]}>
           <option value="">Choose an option</option>
           {#each schema[field].constraints.inclusion as opt}
             <option>{opt}</option>
           {/each}
         </Select>
       {:else if schema[field].type === 'datetime'}
-        <DatePicker bind:value={row[field]} />
+        <DatePicker bind:value={$store.data[field]} />
       {:else if schema[field].type === 'boolean'}
         <Toggle
           text={wide ? null : capitalise(schema[field].name)}
-          bind:checked={row[field]} />
+          bind:checked={$store.data[field]} />
       {:else if schema[field].type === 'number'}
-        <Input type="number" bind:value={row[field]} />
+        <Input type="number" bind:value={$store.data[field]} />
       {:else if schema[field].type === 'string'}
-        <Input bind:value={row[field]} />
+        <Input bind:value={$store.data[field]} />
       {:else if schema[field].type === 'attachment'}
-        <Dropzone bind:files={row[field]} />
+        <Dropzone bind:files={$store.data[field]} />
       {:else if schema[field].type === 'link'}
         <LinkedRowSelector
           secondary
           showLabel={false}
-          bind:linkedRows={row[field]}
+          bind:linkedRows={$store.data[field]}
           schema={schema[field]} />
       {/if}
     </div>
