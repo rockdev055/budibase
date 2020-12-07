8602275d3569991d0473407c4edc0cd655b159e4
rockdev55 <steve264225@gmail.com>
2020-12-07 21:28:16 +0000

    rename level to role in test

--- packages/builder/src/components/userInterface/ComponentNavigationTree/PathTree.svelte
+++ packages/builder/src/components/userInterface/ComponentNavigationTree/PathTree.svelte
@@ -1,10 +1,10 @@
 <script>
   import { writable } from "svelte/store"
   import { goto } from "@sveltech/routify"
-  import { store } from "builderStore"
+  import { store, selectedComponent, currentAsset } from "builderStore"
   import instantiateStore from "./dragDropStore"
 
-  import ComponentsTree from "./ComponentTree.svelte"
+  import ComponentTree from "./ComponentTree.svelte"
   import NavItem from "components/common/NavItem.svelte"
   import ScreenDropdownMenu from "./ScreenDropdownMenu.svelte"
 
@@ -14,12 +14,12 @@
   export let path
   export let indent
 
-  $: selectedScreen = $store.currentPreviewItem
+  $: selectedScreen = $currentAsset
 
   const changeScreen = screenId => {
     // select the route
     store.actions.screens.select(screenId)
-    $goto(`./:page/${screenId}`)
+    $goto(`./${screenId}`)
   }
 </script>
 
@@ -34,17 +34,17 @@
     <NavItem
       icon="ri-artboard-2-line"
       indentLevel={indent || 1}
-      selected={$store.currentPreviewItem._id === screenId}
-      opened={$store.currentPreviewItem._id === screenId}
+      selected={$store.currentAssetId === screenId}
+      opened={$store.currentAssetId === screenId}
       text={url === '/' ? 'Home' : url}
       withArrow={route.subpaths}
       on:click={() => changeScreen(screenId)}>
-      <ScreenDropdownMenu screen={screenId} />
+      <ScreenDropdownMenu {screenId} />
     </NavItem>
     {#if selectedScreen?._id === screenId}
-      <ComponentsTree
+      <ComponentTree
         components={selectedScreen.props._children}
-        currentComponent={$store.currentComponentInfo}
+        currentComponent={$selectedComponent}
         {dragDropStore} />
     {/if}
   {/each}
