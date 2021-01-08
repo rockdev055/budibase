bc0a6510c3c65a4b10cfd7bbab2d2413c23877dd
rockdev55 <steve264225@gmail.com>
2021-01-08 17:25:06 +0000

    Trigger Automation from frontend actions

--- packages/client/src/sdk.js
+++ packages/client/src/sdk.js
@@ -2,6 +2,7 @@ import * as API from "./api"
 import { authStore, routeStore, screenStore, bindingStore } from "./store"
 import { styleable } from "./utils/styleable"
 import { linkable } from "./utils/linkable"
+import { getAppId } from "./utils/getAppId"
 import DataProvider from "./components/DataProvider.svelte"
 
 export default {
@@ -11,6 +12,7 @@ export default {
   screenStore,
   styleable,
   linkable,
+  getAppId,
   DataProvider,
   setBindableValue: bindingStore.actions.setBindableValue,
 }
