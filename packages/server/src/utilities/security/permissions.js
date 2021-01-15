26ef21350a2f596e43c06e6c7fed9474971cef4b
rockdev55 <steve264225@gmail.com>
2021-01-15 14:23:27 +0000

    Update and improve state <-> URL binding

--- packages/server/src/utilities/security/permissions.js
+++ packages/server/src/utilities/security/permissions.js
@@ -14,7 +14,6 @@ const PermissionTypes = {
   WEBHOOK: "webhook",
   BUILDER: "builder",
   VIEW: "view",
-  QUERY: "query",
 }
 
 function Permission(type, level) {
@@ -58,7 +57,6 @@ exports.BUILTIN_PERMISSIONS = {
     _id: exports.BUILTIN_PERMISSION_IDS.READ_ONLY,
     name: "Read only",
     permissions: [
-      new Permission(PermissionTypes.QUERY, PermissionLevels.READ),
       new Permission(PermissionTypes.TABLE, PermissionLevels.READ),
       new Permission(PermissionTypes.VIEW, PermissionLevels.READ),
     ],
@@ -67,7 +65,6 @@ exports.BUILTIN_PERMISSIONS = {
     _id: exports.BUILTIN_PERMISSION_IDS.WRITE,
     name: "Read/Write",
     permissions: [
-      new Permission(PermissionTypes.QUERY, PermissionLevels.WRITE),
       new Permission(PermissionTypes.TABLE, PermissionLevels.WRITE),
       new Permission(PermissionTypes.VIEW, PermissionLevels.READ),
     ],
@@ -92,7 +89,6 @@ exports.BUILTIN_PERMISSIONS = {
       new Permission(PermissionTypes.AUTOMATION, PermissionLevels.ADMIN),
       new Permission(PermissionTypes.VIEW, PermissionLevels.ADMIN),
       new Permission(PermissionTypes.WEBHOOK, PermissionLevels.READ),
-      new Permission(PermissionTypes.QUERY, PermissionLevels.ADMIN),
     ],
   },
 }
